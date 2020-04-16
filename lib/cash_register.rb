8f54a591f7b118dd30f218a47a145485c3d7dd18
Matt Fleming <matthewfleming8@gmail.com>
2020-04-16 22:27:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, price, quantity = 1)
+  def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
     if quantity > 1
